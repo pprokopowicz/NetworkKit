b73a022599e3ae641a1f30eb9ee4299ed136b293
Piotr Prokopowicz <p.prokopowicz1@gmail.com>
2020-12-08 20:25:31 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Networking",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .watchOS(.v5)
+    ],
+    products: [
+        .library(
+            name: "Networking",
+            targets: ["Networking"]),
+    ],
+    targets: [
+        .target(
+            name: "Networking",
+            dependencies: []),
+        .testTarget(
+            name: "NetworkingTests",
+            dependencies: ["Networking"]),
+    ]
+)
