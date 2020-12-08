cf7deb5f1550c4e6d77de050c50dbc8eb3eae8f5
Piotr Prokopowicz <p.prokopowicz1@gmail.com>
2020-12-08 20:33:37 +0100

    Example project added

---
+++ NetworkingExample/NetworkingExample/NetworkingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingExampleApp.swift
+//  NetworkingExample
+//
+//  Created by Piotr Prokopowicz on 08/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
