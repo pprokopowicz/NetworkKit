39fc72e59fd658e476259322c604d7dbab9d3800
Piotr Prokopowicz <p.prokopowicz1@gmail.com>
2020-12-11 23:55:04 +0100

    HTTPMethod enum added

---
+++ Sources/Networking/HTTPMethod.swift
@@ -0,0 +1,24 @@
+//
+//  HTTPMethod.swift
+//  
+//
+//  Created by Piotr Prokopowicz on 08/12/2020.
+//
+
+/**
+ Enum type representing possible HTTP methods.
+ */
+public enum HTTPMethod: String {
+    
+    /// `GET` method.
+    case get = "GET"
+    /// `PUT` method.
+    case put = "PUT"
+    /// `POST` method.
+    case post = "POST"
+    /// `DELETE` method.
+    case delete = "DELETE"
+    /// `PATCH` method.
+    case path = "PATCH"
+    
+}
