11b9a4e0c27bce7b4e13a53ef046f490bd618c84
Piotr Prokopowicz <p.prokopowicz1@gmail.com>
2020-12-11 23:56:00 +0100

    NetworkingBase added

---
+++ Sources/Networking/NetworkingBase.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkingBase.swift
+//  
+//
+//  Created by Piotr Prokopowicz on 11/12/2020.
+//
+
+import Foundation
+
+public protocol NetworkingBase {
+    
+    var url: String { get }
+    
+}
