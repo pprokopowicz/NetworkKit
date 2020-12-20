8d59ac591496302cdaf2dcbe10258ea9e5990a0a
Piotr Prokopowicz <p.prokopowicz1@gmail.com>
2020-12-20 20:33:43 +0100

    Correct error used in request method

--- Sources/Networking/Client/Networking+Request.swift
+++ Sources/Networking/Client/Networking+Request.swift
@@ -16,7 +16,7 @@ extension Networking {
     /// - Returns: AnyPublisher with given services output type or an error. In case of Networking error it will be of type `NetworkingError`.
     public func request<Service: NetworkingService>(service: Service) -> AnyPublisher<Service.Output, Error> {
         guard let urlRequest = URLRequest(service: service, encoder: encoder, timeout: timeout) else {
-            return Fail(error: NetworkingError<NetworkingEmpty>(status: .unableToParseResponse, response: nil))
+            return Fail(error: NetworkingError<NetworkingEmpty>(status: .unableToParseRequest, response: nil))
                 .eraseToAnyPublisher()
         }
         
