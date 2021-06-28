db4102e0bed030bdecda93f438f37057c7bf82d6
Piotr Prokopowicz <p.prokopowicz1@gmail.com>
2021-06-28 17:21:33 +0200

    macOS, tvOS, watchOS example targets added

---
+++ NetworkingExample/NetworkingExampleMacOS/NetworkingExampleMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingExampleMacOSApp.swift
+//  NetworkingExampleMacOS
+//
+//  Created by Piotr Prokopowicz on 28/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingExampleMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NetworkingExample/NetworkingExampleTvOS/NetworkingExampleTvOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingExampleTvOSApp.swift
+//  NetworkingExampleTvOS
+//
+//  Created by Piotr Prokopowicz on 28/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingExampleTvOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
