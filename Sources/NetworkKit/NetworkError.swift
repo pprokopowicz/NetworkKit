45eb0a8eaa7bb1fe1acbb5537c22c237c36bff1e
Piotr Prokopowicz <p.prokopowicz1@gmail.com>
2021-12-28 21:53:21 +0100

    Unused NetworkResponseStatus cases removed

--- Sources/NetworkKit/NetworkError.swift
+++ Sources/NetworkKit/NetworkError.swift
@@ -39,10 +39,7 @@ public struct NetworkError<Response: Decodable>: Error {
 public enum NetworkResponseStatus: Int {
     
     case unknown                        = -1
-    case networkUnreachable             = 0
-    
-    case unableToParseResponse          = 1
-    case unableToParseRequest           = 2
+    case unableToParseRequest           = -2
     
     // 1xx Informational
     case continueError                  = 100
@@ -121,52 +118,5 @@ public enum NetworkResponseStatus: Int {
     case loopDetected                   = 508
     case notExtended                    = 510
     case networkAuthenticationRequired  = 511
-    
-    // domain
-    case cancelled                      = -999
-    case badURL                         = -1000
-    case timedOut                       = -1001
-    case unsupportedURL                 = -1002
-    case cannotFindHost                 = -1003
-    case cannotConnectToHost            = -1004
-    case networkConnectionLost          = -1005
-    case dnsLookupFailed                = -1006
-    case httpTooManyRedirects           = -1007
-    case resourceUnavailable            = -1008
-    case notConnectedToInternet         = -1009
-    case redirectToNonExistentLocation  = -1010
-    case badServerResponse              = -1011
-    case userCancelledAuthentication    = -1012
-    case userAuthenticationRequired     = -1013
-    case zeroByteResource               = -1014
-    case cannotDecodeRawData            = -1015
-    case cannotDecodeContentData        = -1016
-    case cannotParseResponse            = -1017
-    case appTransportSecurityRequiresSecureConnection = -1022
-    case fileDoesNotExist               = -1100
-    case fileIsDirectory                = -1101
-    case noPermissionsToReadFile        = -1102
-    case dataLengthExceedsMaximum       = -1103
-    
-    // SSL errors
-    case secureConnectionFailed         = -1200
-    case serverCertificateHasBadDate    = -1201
-    case serverCertificateUntrusted     = -1202
-    case serverCertificateHasUnknownRoot = -1203
-    case serverCertificateNotYetValid   = -1204
-    case clientCertificateRejected      = -1205
-    case CclientCertificateRequired     = -1206
-    
-    case cannotLoadFromNetwork          = -2000
-    
-    // Download and file I/O errors
-    case cannotCreateFile               = -3000
-    case cannotOpenFile                 = -3001
-    case cannotCloseFile                = -3002
-    case cannotWriteToFile              = -3003
-    case cannotRemoveFile               = -3004
-    case cannotMoveFile                 = -3005
-    case downloadDecodingFailedMidStream = -3006
-    case downloadDecodingFailedToComplete = -3007
     
 }
