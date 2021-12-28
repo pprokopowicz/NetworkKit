9a8cfe150f559c6a46145b7fd680cf95d5adf8b4
Piotr Prokopowicz <p.prokopowicz1@gmail.com>
2021-12-28 19:47:50 +0100

    Example project added back

---
+++ NetworkKitExample/NetworkKitExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NetworkKitExample
+//
+//  Created by Piotr Prokopowicz on 28/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetworkKitExample/NetworkKitExample/NetworkKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkKitExampleApp.swift
+//  NetworkKitExample
+//
+//  Created by Piotr Prokopowicz on 28/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct NetworkKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
